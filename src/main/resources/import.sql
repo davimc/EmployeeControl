ec82e88d408d456691d76380094ff05b0b860a36
davimc <davimatosc@hotmail.com>
2025-01-30 16:13:59 -0300

    Updating user role

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -23,7 +23,7 @@ INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
 
 INSERT INTO tb_employee_role (employee_id, role_id) VALUES (1, 1);
-INSERT INTO tb_employee_role (employee_id, role_id) VALUES (2, 1);
+INSERT INTO tb_employee_role (employee_id, role_id) VALUES (2, 2);
 INSERT INTO tb_employee_role (employee_id, role_id) VALUES (3, 1);
 INSERT INTO tb_employee_role (employee_id, role_id) VALUES (4, 1);
 INSERT INTO tb_employee_role (employee_id, role_id) VALUES (4, 2);
