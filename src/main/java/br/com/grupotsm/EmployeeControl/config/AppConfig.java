94434a43337851c60499b81ef7742381f5189122
davimc <davimatosc@hotmail.com>
2025-01-31 17:01:49 -0300

    Bean RestTemplate

--- src/main/java/br/com/grupotsm/EmployeeControl/config/AppConfig.java
+++ src/main/java/br/com/grupotsm/EmployeeControl/config/AppConfig.java
@@ -1,15 +1,34 @@
 package br.com.grupotsm.EmployeeControl.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class AppConfig {
+
+    /*@Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @Autowired
+    private UserDetailsService userDetailsService;*/
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+/*    @Bean
+    protected AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(passwordEncoder);
+        return new ProviderManager(provider);
+    }*/
+
 }
