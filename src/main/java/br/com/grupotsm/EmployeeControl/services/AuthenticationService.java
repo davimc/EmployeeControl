2347b8f9b79f1dd17a13bf9000161678f06285f8
davimc <davimatosc@hotmail.com>
2025-01-31 17:06:15 -0300

    cleaning

--- src/main/java/br/com/grupotsm/EmployeeControl/services/AuthenticationService.java
+++ src/main/java/br/com/grupotsm/EmployeeControl/services/AuthenticationService.java
@@ -1,6 +1,5 @@
 package br.com.grupotsm.EmployeeControl.services;
 
-import br.com.grupotsm.EmployeeControl.DTO.auth.Login;
 import br.com.grupotsm.EmployeeControl.DTO.auth.LoginDTO;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
